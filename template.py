6bd469753afa9f8e0336176cb1e70944d3db5aa7
Prashanta Kumar Mithu <proshanta.mithu5@gmail.com>
2025-10-03 22:19:19 +0600

    Folder strecture updated

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
 
-project_name = "chestCancerClassifier"
+project_name = "KidneyCNN"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
